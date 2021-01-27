1c112715a544f095aa3639df906e6c454177815d
Feeliks Kilpi <feelikskilpi@Feeliks-MacBook-Air.local>
2021-01-27 22:58:41 +0200

    Tram position messages near certain tram stops

--- ratikat.js
+++ ratikat.js
@@ -2,11 +2,14 @@ const mqtt = require('mqtt')
 const topic = '/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/+/60;24/19/62/01/#';
 const HSLclient  = mqtt.connect('mqtts://mqtt.hsl.fi:8883');
 
+// Function that ends connection to HSL client. Called when setTimeout reaches 15mins
 function stopClient() {
+  console.log("Timeout, closing connection automatically...");
   HSLclient.end();
 }
 // /<prefix>/<version>/<journey_type>/<temporal_type>/<event_type>/<transport_mode>/<operator_id>/<vehicle_number>/<route_id>/<direction_id>/<headsign>/<start_time>/<next_stop>/<geohash_level>/<geohash>/<sid>/#
 
+// Connect to HSL client
 HSLclient.on('connect', function () {
   HSLclient.subscribe(topic, function (err) {
     if (!err) {
@@ -17,19 +20,96 @@ HSLclient.on('connect', function () {
   })
 });
 
+// Subscribe to various topics consisting of selected locations near tram stops at Huutokonttori, Jätkäsaari
 HSLclient.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/+/60;24/19/61/09/#");
 HSLclient.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/+/60;24/19/52/91/#");
 HSLclient.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/+/60;24/19/62/00/#");
 HSLclient.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/+/60;24/19/62/00/#");
 HSLclient.subscribe("/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/+/60;24/19/51/98/#");
 
-setTimeout(stopClient, 3600000);
+// Set automatic timeout to close client after 15 minutes
+setTimeout(stopClient, 900000);
 
-
+// When message comes from some of the subscribed topics
 HSLclient.on('message', function (topic, message) {
-  console.log("New message!\n");
+  // Create timestamp
+  var currentdate = new Date(); 
+  var datetime =    "<"+currentdate.getDate() + "/"
+                  + (currentdate.getMonth()+1)  + "/" 
+                  + currentdate.getFullYear() + " @ "  
+                  + currentdate.getHours() + ":"  
+                  + currentdate.getMinutes() + ":" 
+                  + currentdate.getSeconds() + ">";
+  // json object of each message, message parsed to json object
   let json = JSON.parse(message.toString());
-  let speed = json.VP.spd * 3.6;
-  console.log('Ratikka ' + json.VP.desi + ', kulkee suuntaan ' + json.VP.dir + ' nopeudella: ' + speed + ' km/h!');
+  // Different attributes of message, incl. line number, speed, direction, and door status
+  let line = json.VP.desi;
+  let speed = Math.round(json.VP.spd * 3.6);
+  let direction = json.VP.dir;
+  let doors = json.VP.drst;
+
+  // Conditional statement for different tram lines
+  switch (line) {
+
+    case "9":
+      if (direction === "1") {
+        console.log(datetime + ' ' + 'Tram ' + line + ', is heading towards Pasila at speed: ' + speed + ' km/h!');
+        if (speed <= 1 && doors === 0) {
+          console.log("Tram is at the stop and doors are closed");
+        }
+        else if (speed <= 1 && doors === 1) {
+          console.log("Tram is at the stop and doors are open");
+        }
+      }
+      else if (direction === "2") {
+        console.log(datetime + ' ' + 'Tram ' + line + ', is heading towards Jätkäsaari at speed: ' + speed + ' km/h!');
+      }
+      console.log("\n");
+      break;
+
+    case "7":
+      if (direction === "1") {
+        console.log(datetime + ' ' + 'Tram ' + line + ', is heading towards Pasila at speed: ' + speed + ' km/h!');
+        if (speed <= 1 && doors === 0) {
+          console.log("Tram is at the stop and doors are closed");
+        }
+        else if (speed <= 1 && doors === 1) {
+          console.log("Tram is at the stop and doors are open");
+        }
+      }
+      else if (direction === "2") {
+        console.log(datetime + ' ' + 'Tram ' + line + ', is heading towards Länsiterminaali at speed: ' + speed + ' km/h!');
+        if (speed <= 1 && doors === 0) {
+          console.log("Tram is at the stop and doors are closed");
+        }
+        else if (speed <= 1 && doors === 1) {
+          console.log("Tram is at the stop and doors are open");
+        }
+      }
+      console.log("\n");
+      break;
+
+    case "6T":
+      if (direction === "1") {
+        console.log(datetime + ' ' + 'Tram ' + line + ', is heading towards Arabianranta at speed: ' + speed + ' km/h!');
+        if (speed <= 1 && doors === 0) {
+          console.log("Tram is at the stop and doors are closed");
+        }
+        else if (speed <= 1 && doors === 1) {
+          console.log("Tram is at the stop and doors are open");
+        }
+      }
+      else if (direction === "2") {
+        console.log(datetime + ' ' + 'Tram ' + line + ', is heading towards Länsiterminaali at speed: ' + speed + ' km/h!');
+        if (speed <= 1 && doors === 0) {
+          console.log("Tram is at the stop and doors are closed");
+        }
+        else if (speed <= 1 && doors === 1) {
+          console.log("Tram is at the stop and doors are open");
+        }
+      }
+      console.log("\n");
+      break;
+  }
      
 });
